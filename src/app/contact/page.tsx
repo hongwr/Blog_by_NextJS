8fe178d007c40ffeaa7a48ececb172dbebda7100
HongWangyeol <109332847+HongWangyeol@users.noreply.github.com>
2023-10-05 19:09:10 +0900

    css 수정중

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function ContactPage() {
+    return <div>나야나</div>;
+}
---
+++ src/components/Footer.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Footer() {
+    return <p className='bg-slate-900 text-white py-2 text-sm text-center'>Don&rsquo;t forget to CODE your DREAM | ALL Right Reserved</p>;
+}
