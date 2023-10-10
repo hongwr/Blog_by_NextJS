498f1189dc432979b6c2d83a38244b2c809ccaba
HongWangyeol <109332847+HongWangyeol@users.noreply.github.com>
2023-10-10 17:48:33 +0900

    add post

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import { getAllPosts } from '@/api/posts';
+import FilterablePosts from '@/components/FilterablePosts';
 
-export default function AboutPage() {
-    return <div>post</div>;
+export default async function PostsPage() {
+    const posts = await getAllPosts();
+    const categories = [...new Set(posts.map((post) => post.category))];
+
+    return <FilterablePosts posts={posts} categories={categories} />;
 }
