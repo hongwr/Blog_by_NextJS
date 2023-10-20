412d45f4de3a216094a40f19bbba2dca3a8791b6
HongWangyeol <hongwr88@gmail.com>
2023-10-20 18:04:30 +0900

    add send email in client

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -2,6 +2,7 @@
 
 import { ChangeEvent, FormEvent, useState } from 'react';
 import Banner, { BannerData } from './Banner';
+import { sendContactEmail } from '@/api/contact';
 
 type Form = {
     from: string;
@@ -9,21 +10,36 @@ type Form = {
     message: string;
 };
 
+const DEFAULT_DATA = {
+    from: '',
+    subject: '',
+    message: '',
+};
+
 export default function ContactForm() {
-    const [form, setForm] = useState<Form>({ from: '', subject: '', message: '' });
+    const [form, setForm] = useState<Form>(DEFAULT_DATA);
     const [banner, setBanner] = useState<BannerData | null>(null);
 
     const onChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         const { name, value } = e.target;
         setForm((prev) => ({ ...prev, [name]: value }));
     };
+    // email 보내기 요청
     const onSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        console.log(form);
-        setBanner({ message: '성공했어!!', state: 'success' });
-        setTimeout(() => {
-            setBanner(null);
-        }, 3000);
+        sendContactEmail(form)
+            .then(() => {
+                setBanner({ message: '메일을 성공적으로 보냈습니다.', state: 'success' });
+                setForm(DEFAULT_DATA);
+            })
+            .catch(() => {
+                setBanner({ message: '메일 전송에 실패하였습니다. 다시 시도해 주세요.', state: 'error' });
+            })
+            .finally(() => {
+                setTimeout(() => {
+                    setBanner(null);
+                }, 3000);
+            });
     };
 
     return (
