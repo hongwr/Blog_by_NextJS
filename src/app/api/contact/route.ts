4de09833185792a1209c418318406403bce14efd
HongWangyeol <hongwr88@gmail.com>
2023-10-26 17:29:44 +0900

    add nodemailer and sendEmail

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,3 +1,4 @@
+import { sendEmail } from '@/api/email';
 import * as yup from 'yup';
 
 const bodySchema = yup.object().shape({
@@ -7,9 +8,16 @@ const bodySchema = yup.object().shape({
 });
 
 export async function POST(req: Request) {
-    if (!bodySchema.isValidSync(req.body)) {
-        return new Response('유효하지 않은 포맷', { status: 400 });
+    const body = await req.json();
+    console.log(body);
+    if (!bodySchema.isValidSync(body)) {
+        return new Response(JSON.stringify({ message: '메일 전송에 실패함!' }), { status: 400 });
     }
-    const { from, subject, message } = req.body;
-    // 노드 메일러를 이용해서 메일 전송
+
+    return sendEmail(body)
+        .then(() => new Response(JSON.stringify({ message: '메일을 성공적으로 보냈음' }), { status: 200 }))
+        .catch((error) => {
+            console.error(error);
+            return new Response(JSON.stringify({ message: '메일 전송에 실패함!' }), { status: 500 });
+        });
 }
